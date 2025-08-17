e5a5e26c24408d5349456c6abbcc0b21e5d0c1d1
Ghaith-aljabban <ghaithaljabban2005@gmail.com>
2025-08-17 12:08:31 +0300

    upcome exam + fix the schedule

--- lib/models/exam_date_model.dart
+++ lib/models/exam_date_model.dart
@@ -1,18 +1,27 @@
-import 'package:school_application/Models/subjects_model.dart';
-
-import '../main.dart';
-
 class ExamDateModel {
-  SubjectsModel subject;
+  String subjectName; // Simplified to just the name
   String date;
   String timeStart;
   String timeEnd;
+
   ExamDateModel({
-    required this.subject,
+    required this.subjectName,
     required this.date,
     required this.timeStart,
     required this.timeEnd,
   });
+
+  factory ExamDateModel.fromJson(Map<String, dynamic> json) {
+    final start = DateTime.parse(json['start_datetime']);
+    final end = DateTime.parse(json['end_datetime']);
+
+    return ExamDateModel(
+      subjectName: json['subject_name'],
+      date: "${start.day}/${start.month}/${start.year}",
+      timeStart: "${start.hour}:${start.minute.toString().padLeft(2, '0')}",
+      timeEnd: "${end.hour}:${end.minute.toString().padLeft(2, '0')}",
+    );
+  }
 }
 
 List<ExamDateModel> myExams = [];
